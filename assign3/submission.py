7fceac063b3202b949fc15e65bd62032470450ea
Gwanho Kim <khkim6040@postech.ac.kr>
2024-03-22 11:04:44 +0900

    Upload assign3 submission.py

--- assign3/submission.py
+++ assign3/submission.py
@@ -16,17 +16,24 @@ class WordSegmentationProblem(util.SearchProblem):
 
     def startState(self):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return self.query
         # END_YOUR_ANSWER
 
     def isEnd(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return len(state) == 0
         # END_YOUR_ANSWER
 
     def succAndCost(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 5 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        # split one by one from starting index
+        result = []
+        for i in range(1, len(state)+1):
+            action = state[:i]
+            new_state = state[i:]
+            cost = self.unigramCost(action)
+            result.append((action, new_state, cost))
+        return result
         # END_YOUR_ANSWER
 
 def segmentWords(query, unigramCost):
@@ -37,7 +44,7 @@ def segmentWords(query, unigramCost):
     ucs.solve(WordSegmentationProblem(query, unigramCost))
 
     # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    return ' '.join(ucs.actions)
     # END_YOUR_ANSWER
 
 # Problem 1b: Solve the k-word segmentation problem under a unigram model
@@ -50,17 +57,35 @@ class KWordSegmentationProblem(util.SearchProblem):
 
     def startState(self):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        # (받은 string, split 횟수)
+        return (self.query, 0)
         # END_YOUR_ANSWER
 
     def isEnd(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return len(state[0]) == 0 and state[1] == self.k
         # END_YOUR_ANSWER
 
     def succAndCost(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 7 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        curStr, splitCount = state
+        result = []
+        if splitCount == self.k-1:
+            # If only one split is left, split the remaining string and calculate the cost
+            action = curStr
+            newState = ("", splitCount+1)
+            cost = self.unigramCost(action)
+            result.append((action, newState, cost))
+            return result
+        
+        for i in range(1, len(curStr)+1):
+            # If more than one split is left, split the string at the current position and continue with the rest of the string
+            action = curStr[:i]
+            cost = self.unigramCost(action)
+            newState = (curStr[i:], splitCount+1)
+            result.append((action, newState, cost))
+            
+        return result
         # END_YOUR_ANSWER
 
 def segmentKWords(k, query, unigramCost):
@@ -68,7 +93,9 @@ def segmentKWords(k, query, unigramCost):
         return ''
 
     # BEGIN_YOUR_ANSWER (our solution is 3 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    ucs = util.UniformCostSearch()
+    ucs.solve(KWordSegmentationProblem(k, query, unigramCost))
+    return ' '.join(ucs.actions)
     # END_YOUR_ANSWER
 
 ############################################################
@@ -84,22 +111,35 @@ class VowelInsertionProblem(util.SearchProblem):
 
     def startState(self):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        # bigram을 사용하려면 이전 단어 필요함. state = (다음 index, 이전 단어)로 구성
+        return (0, wordsegUtil.SENTENCE_BEGIN)
         # END_YOUR_ANSWER
 
     def isEnd(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return state[0] == len(self.queryWords)
         # END_YOUR_ANSWER
 
     def succAndCost(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 9 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        curIndex, preWord = state
+        curWord = self.queryWords[curIndex]
+        possibleWords = self.possibleFills(curWord)
+        result = []
+        # if curWord is not in possibleFills(), curWord will be put in possibleWords itself
+        if not possibleWords:
+            possibleWords = {curWord}
+        for word in possibleWords:
+            cost = self.bigramCost(preWord, word)
+            result.append((word, (curIndex+1, word), cost))
+        return result
         # END_YOUR_ANSWER
 
 def insertVowels(queryWords, bigramCost, possibleFills):
     # BEGIN_YOUR_ANSWER (our solution is 3 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    ucs = util.UniformCostSearch()
+    ucs.solve(VowelInsertionProblem(queryWords, bigramCost, possibleFills))
+    return ' '.join(ucs.actions)
     # END_YOUR_ANSWER
 
 # Problem 2b: Solve the limited vowel insertion problem under a bigram cost
@@ -113,22 +153,37 @@ class LimitedVowelInsertionProblem(util.SearchProblem):
 
     def startState(self):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return (0, wordsegUtil.SENTENCE_BEGIN)
         # END_YOUR_ANSWER
 
     def isEnd(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return state[0] == len(self.queryWords)
         # END_YOUR_ANSWER
 
     def succAndCost(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 10 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        curIndex, preWord = state
+        curWord = self.queryWords[curIndex]
+        possibleWords = self.possibleFills(curWord)
+
+        for vowel in self.impossibleVowels:
+            possibleWords = set.intersection(possibleWords, {word for word in possibleWords if vowel not in word})
+        if not possibleWords:
+            possibleWords = {curWord}
+            
+        result = []
+        for word in possibleWords:
+            cost = self.bigramCost(preWord, word)
+            result.append((word, (curIndex+1, word), cost))
+        return result
         # END_YOUR_ANSWER
 
 def insertLimitedVowels(impossibleVowels, queryWords, bigramCost, possibleFills):
     # BEGIN_YOUR_ANSWER (our solution is 3 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    ucs = util.UniformCostSearch()
+    ucs.solve(LimitedVowelInsertionProblem(impossibleVowels, queryWords, bigramCost, possibleFills))
+    return ' '.join(ucs.actions)
     # END_YOUR_ANSWER
 
 ############################################################
@@ -144,17 +199,27 @@ class JointSegmentationInsertionProblem(util.SearchProblem):
 
     def startState(self):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return (0, wordsegUtil.SENTENCE_BEGIN)
         # END_YOUR_ANSWER
 
     def isEnd(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return state[0] == len(self.query)
         # END_YOUR_ANSWER
 
     def succAndCost(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 8 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        # query의 index 하나씩 늘려가면서 가능한 단어들 모두 cost구해서 result에 추가
+        curIndex, preWord = state
+        result = []
+        for indexTo in range(curIndex+1, len(self.query)+1):
+            curWord = self.query[curIndex:indexTo]
+            possibleWords = self.possibleFills(curWord)
+            for word in possibleWords:
+                cost = self.bigramCost(preWord, word)
+                result.append((word, (indexTo, word), cost))
+        
+        return result
         # END_YOUR_ANSWER
 
 def segmentAndInsert(query, bigramCost, possibleFills):
@@ -162,7 +227,9 @@ def segmentAndInsert(query, bigramCost, possibleFills):
         return ''
 
     # BEGIN_YOUR_ANSWER (our solution is 3 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    ucs = util.UniformCostSearch()
+    ucs.solve(JointSegmentationInsertionProblem(query, bigramCost, possibleFills))
+    return ' '.join(ucs.actions)
     # END_YOUR_ANSWER
 
 ############################################################
@@ -173,27 +240,33 @@ def segmentAndInsert(query, bigramCost, possibleFills):
 class SimpleProblem(util.SearchProblem):
     def __init__(self):
         # BEGIN_YOUR_ANSWER (our solution is 4 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        self.start = 0
+        self.end = 4
+        self.path = [[(1, 2), (2, 1)], [(3, 3)], [(3, 1)], [(4, 100)]]
         # END_YOUR_ANSWER
 
     def startState(self):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return self.start
         # END_YOUR_ANSWER
 
     def isEnd(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return state == self.end
         # END_YOUR_ANSWER
 
     def succAndCost(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 3 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        result = []
+        for action, cost in self.path[state]:
+            result.append(("Move {0} to {1}".format(state, action), action, cost))
+        return result
         # END_YOUR_ANSWER
 
 def admissibleButInconsistentHeuristic(state):
     # BEGIN_YOUR_ANSWER (our solution is 2 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    heuristicCost = {0:1, 1:1, 2:100, 3:1, 4:0}
+    return heuristicCost[state]
     # END_YOUR_ANSWER
 
 # Problem 4b: Apply a heuristic function to the joint segmentation-and-insertion problem
@@ -205,7 +278,14 @@ def makeWordCost(bigramCost, wordPairs):
     :returns: wordCost, which is a function from word to cost
     """
     # BEGIN_YOUR_ANSWER (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    wordCostDict = {} # key: word, value: minimum cost
+    for w1, w2 in wordPairs:
+        wordCostDict[w2] = min(bigramCost(w1, w2), wordCostDict[w2]) if w2 in wordCostDict.keys() else bigramCost(w1, w2)
+        
+    def wordCost(word):
+        return wordCostDict[word] if word in wordCostDict.keys() else bigramCost(wordsegUtil.SENTENCE_UNK, word)
+    
+    return wordCost
     # END_YOUR_ANSWER
 
 class RelaxedProblem(util.SearchProblem):
@@ -216,22 +296,33 @@ class RelaxedProblem(util.SearchProblem):
 
     def startState(self):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return 0
         # END_YOUR_ANSWER
 
     def isEnd(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 1 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        return state == len(self.query)
         # END_YOUR_ANSWER
 
     def succAndCost(self, state):
         # BEGIN_YOUR_ANSWER (our solution is 5 lines of code, but don't worry if you deviate from this)
-        raise NotImplementedError  # remove this line before writing code
+        result = []
+        # index 1씩 증가
+        for indexTo in range(state+1, len(self.query)+1):
+            # 해당 범위에서 가능한 모든 단어 취하기
+            for word in self.possibleFills(self.query[state:indexTo]):
+                result.append((word, indexTo, self.wordCost(word)))
+        return result
         # END_YOUR_ANSWER
 
 def makeHeuristic(query, wordCost, possibleFills):
     # BEGIN_YOUR_ANSWER (our solution is 2 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    problem = RelaxedProblem(query, wordCost, possibleFills)
+    dpHelper = util.DynamicProgramming(problem)
+    def heuristic(state):
+        return dpHelper(state[0])
+    
+    return heuristic
     # END_YOUR_ANSWER
 
 def fastSegmentAndInsert(query, bigramCost, wordCost, possibleFills):
@@ -239,8 +330,11 @@ def fastSegmentAndInsert(query, bigramCost, wordCost, possibleFills):
         return ''
 
     # BEGIN_YOUR_ANSWER (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    ucs = util.UniformCostSearch()
+    ucs.solve(JointSegmentationInsertionProblem(query, bigramCost, possibleFills), makeHeuristic(query, wordCost, possibleFills))
+    return ' '.join(ucs.actions)
     # END_YOUR_ANSWER
+    
 
 ############################################################
 
